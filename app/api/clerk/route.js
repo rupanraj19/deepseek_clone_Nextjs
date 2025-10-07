726f247fc952dc2b3877df60a4c0b89add5c8cb9
ruby <105553566+rupanraj19@users.noreply.github.com>
2025-10-07 13:55:44 +0800

    update image_url in route js

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -24,7 +24,7 @@ export async function POST(req){
         _id: data.id,
         email: data.email_addresses[0].email_address,
         name: `${data.first_name} ${data.last_name}`,
-        image: data.image.url,
+        image: data.image_url,
     }
 
     await connectDB();
